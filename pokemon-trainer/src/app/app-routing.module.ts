bf660a8d8a31a7521f3932d9b102a88365a56bd1
Aleksander Røder <fylling8@gmail.com>
2023-03-01 10:57:07 +0100

    add: pokemonDisplay component, and trainer page with routing

--- pokemon-trainer/src/app/app-routing.module.ts
+++ pokemon-trainer/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TrainerComponent } from './trainer/trainer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LandingPageComponent},
+  {path:'pokedex',component:LandingPageComponent},
+  {path:'trainer',component:TrainerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pokemon-trainer/src/app/app.module.ts
+++ pokemon-trainer/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { BackgroundComponent } from './background/background.component';
 import { HeaderComponent } from './header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TrainerComponent } from './trainer/trainer.component';
+import { PokemonDisplayComponent } from './trainer/pokemon-display/pokemon-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BackgroundComponent,
     HeaderComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    TrainerComponent,
+    PokemonDisplayComponent
   ],
   imports: [
     BrowserModule,
