5a05bcbfda15f34d69b15dc6c472cfe4baadeb9a
ARodder <44817628+ARodder@users.noreply.github.com>
2023-03-02 22:16:01 +0100

    change background

--- pokemon-trainer/src/app/background/background.component.ts
+++ pokemon-trainer/src/app/background/background.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./background.component.css']
 })
 export class BackgroundComponent {
-
+  showDawn:boolean = new Date().getHours() >= 20;
 }
